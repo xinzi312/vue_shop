7d10d84dcbde0f4888a8e9ea63262087bbd810db
xinxi <1697858042@qq.com>
2020-06-13 18:01:59 +0800

    完成了登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="logon_container">
         <div class="login_box">
-            <!-- 头像区域 -->
-            <div class="avatar_box">
-                <img src="../assets/logo.png">
-            </div>
+             <div class="avatar_box">
+                 <img src="../assets/logo.png">
+             </div>   
             <!-- 登录表单区域 -->
             <!-- 使用element-ui实现登录表单  -->
-            <el-form ref="loginFormRef"  :rules="loginFormRules" :model="loginForm" label-width="0px" class="login_form">
+       <el-form ref="loginFormRef"  :rules="loginFormRules" :model="loginForm" label-width="0px" class="login_form">
                 <!-- 用户名 -->
-                <!-- el-form-item表示表单项 -->
-                <el-form-item prop="username">
+                 <!-- el-form-item表示表单项 -->
+               <el-form-item prop="username">
                     <el-input v-model="loginForm.username" prefix-icon="icon iconfont icon-user">       
                     </el-input>
                 </el-form-item>
@@ -38,8 +37,7 @@ export default {
                 username:'zs',
                 password:'shu'
             },
-            //这是表单验证规则对象
-            loginFormRules:{
+             loginFormRules:{
                 //验证用户名是否合法
                 username:[
                         // required表示必填项 blur表示鼠标离开的时候校验
@@ -56,35 +54,30 @@ export default {
             },
         }
     },
-      methods:{
+    
+    methods:{
                 //点击重置按钮重置表单
                 resetLoginForm(){
                     this.$refs.loginFormRef.resetFields()
                 },
                 login(){
                     this.$refs.loginFormRef.validate(async valid=>{
-                        // console.log(valid)
-                        if(!valid) return;
-                        //将返回的值解构赋值为一个data属性 将其重命名为res
+                       
+                    if(!valid) return;
+
                       
-                        const {data:res}=await this.$http.post("login",this.loginForm);
-                        //如果某一个方法返回的是Promise可以用await async来简化这次操作
-                        //await只能用在被async修饰的方法中
-                        console.log(res)
-                        if(res.meta.status!==200) return this.$message.error("登录失败！")
-                        this.$message.success('登录成功!')
-                        //1 将登录成功之后的token 保存到客户的seesionStorage中（token为确定登录后服务器提供的令牌 之后访问都要携带token值）
-                        //1 1 项目中除了登录之外的其他API接口 必须在登录之后才能访问
-                        //1.2 token只应当在当前网站打开期间生效 所以将token保存在sesssionStorage中(localStorage是持久化的存储机制 sessionStorage是会话时的存储机制)
-                              //token属性在res的data属性中
-                          console.log(res)    
-                          window.sessionStorage.setItem('token',res.data.token)    
-                        //2 通过编程式导航跳转到后台主页 路由地址是/home
-                        this.$router.push('/home')
+                    const {data:res}=await this.$http.post("login",this.loginForm);
+
+                    if(res.meta.status!==200) return this.$message.error("登录失败！")
+                    this.$message.success('登录成功!')
+
+                    window.sessionStorage.setItem('token',res.data.token)
+
+                    this.$router.push('/home')
 
                     })
                 }
-            }
+            }        
 }
 </script>
 <style lang="less" scoped>
@@ -121,7 +114,6 @@ export default {
         }
     }
 }
-
 .login_form{
     position: absolute;
     bottom:0;
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,4 @@
-// import Vue from 'vue'
+import Vue from 'vue'
 // import { Button } from 'element-ui'
 
 // import {Form,FormItem} from 'element-ui'
